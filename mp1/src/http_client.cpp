b7a644706b6da5aa09e38766f3f01631fcb6e641
Haoyue Bai <haoyueb2@illinois.edu>
2022-09-18 14:15:28 -0500

    incremented version to 2

--- mp1/src/http_client.cpp
+++ mp1/src/http_client.cpp
@@ -18,7 +18,7 @@
 
 // #define PORT "3490" // the port client will be connecting to 
 
-#define MAXDATASIZE 1000 // max number of bytes we can get at once 
+#define MAXDATASIZE 100000 // max number of bytes we can get at once 
 
 using namespace std;
 

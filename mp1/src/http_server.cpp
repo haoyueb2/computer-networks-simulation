d937d416979fd70fe0cf85a8a34826864f70bf01
Haoyue Bai <haoyueb2@illinois.edu>
2022-09-18 14:02:50 -0500

    complete server part

--- mp1/src/http_server.cpp
+++ mp1/src/http_server.cpp
@@ -1,6 +1,15 @@
 /*
 ** server.c -- a stream socket server demo
 */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <filesystem>
+#include <unistd.h>
+#include <cstdio>
+#include <sys/stat.h>
+using namespace std;
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,9 +26,9 @@
 
 #define PORT "3490"  // the port users will be connecting to
 
-#define BACKLOG 10	 // how many pending connections queue will hold
+#define MAX_MESSAGE_SIZE 100000
 
-#define MAXDATASIZE 100 
+#define BACKLOG 10	 // how many pending connections queue will hold
 
 void sigchld_handler(int s)
 {
@@ -36,7 +45,67 @@ void *get_in_addr(struct sockaddr *sa)
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(void)
+vector<string> split(string str, string deliminator, int times = -1) {
+    vector<string> ret;
+    int counter = 0;
+    int ptr = 0;
+    int next_ptr = 0;
+    while (true) {
+        next_ptr = str.find(deliminator, ptr);
+        if (next_ptr == string::npos) {
+            break;
+        }
+        ret.push_back(str.substr(ptr, next_ptr - ptr));
+        ptr = next_ptr + 1;
+        counter += 1;
+        if (counter == times) {
+            break;
+        }
+    }
+    if (ptr != str.size()) {
+        ret.push_back(str.substr(ptr, str.size() - ptr));
+    }
+    return ret;
+}
+
+string read_file(string file_dir) {
+    char buf[file_dir.size() + 1];
+    strcpy(buf, file_dir.c_str());
+    buf[file_dir.size()] = '\0';
+    string file_content = "";
+    FILE* fp = fopen(buf, "r");
+	if (!fp) {
+		return file_content;
+	}
+    struct stat file_info;
+    stat(file_dir.c_str(), &file_info);
+    file_content.resize(file_info.st_size);
+    fread(const_cast<char*>(file_content.data()), file_info.st_size, 1, fp);
+    return file_content;
+}
+
+string get_http_response(string request) {
+    vector<string> infos = split(request, "\r\n");
+    string response = "";
+    string header = infos[0];
+    auto request_line_infos = split(header, " ");
+    response += "HTTP/1.1 ";
+    if (request_line_infos.size() != 3 || request_line_infos[0] != "GET") {
+        response += "400 Bad Request\r\n\r\n";
+        return response;
+    }
+    string file_dir = request_line_infos[1].substr(1, request_line_infos[1].size()-1);
+    string file_content = read_file(file_dir);
+    if (file_content.size() == 0) {
+        response += "404 Not Found\r\n\r\n";
+        return response;
+    }
+    response += "200 OK\r\n\r\n";
+    response += file_content;
+    return response;
+}
+
+int main(int argc, char *argv[])
 {
 	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
 	struct addrinfo hints, *servinfo, *p;
@@ -46,14 +115,19 @@ int main(void)
 	int yes=1;
 	char s[INET6_ADDRSTRLEN];
 	int rv;
-	char buf[MAXDATASIZE];
+	string port;
+	if (argc != 2) {
+	    fprintf(stderr,"usage: server port\n");
+	    exit(1);
+	}
+	port = argv[1];
 
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE; // use my IP
 
-	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+	if ((rv = getaddrinfo(NULL, port.c_str(), &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
 	}
@@ -118,13 +192,16 @@ int main(void)
 
 		if (!fork()) { // this is the child process
 			close(sockfd); // child doesn't need the listener
-			if (recv(new_fd, buf, MAXDATASIZE ,0) == -1) {
-				perror("recv");
-			}
-			printf("server debug: got message from client %s\n", buf);
-			if (send(new_fd, "Mock Http Response!", 19, 0) == -1) {
+            char buf[MAX_MESSAGE_SIZE];
+            if (recv(new_fd, buf, MAX_MESSAGE_SIZE, 0)) {
+                perror("send");
+            }
+            string response = get_http_response(string(buf));
+            char char_resp[response.size() + 1];
+            strcpy(char_resp, response.c_str());
+            char_resp[response.size()] = '\0';
+			if (send(new_fd, char_resp, response.size(), 0) == -1)
 				perror("send");
-			}
 			close(new_fd);
 			exit(0);
 		}
@@ -133,4 +210,3 @@ int main(void)
 
 	return 0;
 }
-
